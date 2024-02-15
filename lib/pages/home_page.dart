5287162122ceb78fc1960adb83bc6bde75b03d70
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2024-02-15 14:35:44 -0600

    ContadorDeClicks

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePage  extends StatefulWidget {
-  const HomePage ({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int cont =0;
+  int cont = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +18,59 @@ class _HomePageState extends State<HomePage> {
         title: const Text("Contador"),
       ),
       body: Center(
-        child: 
-        Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-             const Text("Número de clicks"),
-              Text("$cont"),
+            const Text("Número de clicks"),
+            Text("$cont"), // Mostrar variables el $
           ],
-        )
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _crearBotones(),
+    );
+  }
+
+  Widget _crearBotones() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () => _sumar(),
+          ),
+          const SizedBox(width: 5),
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () => _restar(),
+          ),
+          const SizedBox(width: 5),
+          FloatingActionButton(
+            child: const Icon(Icons.exposure_zero),
+            onPressed: () => _reset(),
+          ),
+        ],
       ),
     );
+  }
+
+  void _sumar() {
+    setState(() => cont++);
+  }
+
+  void _restar() {
+    setState(() {
+      if (cont > 0) {
+        cont--;
+      }
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      cont = 0;
+    });
   }
 }
